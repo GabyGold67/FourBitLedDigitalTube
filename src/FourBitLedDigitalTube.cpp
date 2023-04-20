67a3b21a92a73c0db56f3a30b864cba4e045067e
Gaby Goldman <82763943+GabyGold67@users.noreply.github.com>
2023-04-20 09:33:15 +0300

    Initial Commit

---
+++ src/FourBitLedDigitalTube.cpp
@@ -0,0 +1,95 @@
+#include <Arduino.h>
+#include <FourBitLedDigitalTube.h>
+
+TM74HC595LedTube::TM74HC595LedTube(int sclk, int rclk, int dio)
+    :_sclk {sclk}, _rclk {rclk}, _dio {dio} 
+{
+  pinMode(_sclk, OUTPUT);
+  pinMode(_rclk, OUTPUT);
+  pinMode(_dio, OUTPUT);
+
+  clear();
+}
+
+void  TM74HC595LedTube::refresh(){
+  send(_data[0], 0b0001);
+  send(_data[1], 0b0010);
+  send(_data[2], 0b0100);
+  send(_data[3], 0b1000);
+  return;
+}
+
+void TM74HC595LedTube::clear(){
+  //Cleans the contents of the internal DATA array display buffer (All leds off for all digits)
+  _data[0]= _data[1]= _data[2]= _data[3]= 0xFF;
+  refresh();
+}
+
+void TM74HC595LedTube::send(unsigned char content)
+{
+  //Sends the byte value (char <=> unsigned short int) to the 4 7-segment display bit by bit
+    shiftOut(_dio, _sclk, MSBFIRST, content);
+}
+
+void TM74HC595LedTube::send(unsigned char segments, unsigned char port)
+// Sends the character 'X' to the digit 'port' of the display
+// Content and Port must be sent in two sequencial parts, character first, port second
+// so this overloaded two char send method uses the one char send method twice and then moves 
+//up the RCLK pin to present the content in the shift register
+{
+  digitalWrite(_rclk, LOW);
+  send(segments);
+  send(port);
+  digitalWrite(_rclk, HIGH);
+}
+
+bool TM74HC595LedTube::print(String text){
+    //Takes text and tries to display it at the LED tube
+    //returns true if found all the characters
+    //returns false if at least one char had no representation
+    //If no fidel representation is posible blanks the display to avoid false info risk
+  
+    bool displayable {true};
+    int position {-1};
+    String tempText {""};
+    int temp7SegData[4] {0xFF, 0xFF, 0xFF, 0xFF};
+    int tempDpData[4] {0xFF, 0xFF, 0xFF, 0xFF};
+  
+    //Finds out if there are '.' in the string to display, creates a mask to add them to the display
+    //and takes them out of the string to process the chars/digits
+    for (unsigned int i{0}; i< text.length(); ++i){
+        if (text.charAt(i) != '.'){
+        tempText += text.charAt(i);
+        }
+        else{
+        if (i == 0 || text.charAt(i-1) == '.')
+            tempText += " ";
+        if(tempText.length()<=4)
+            tempDpData[3-(tempText.length()-1)] = 0x7F;      
+        }
+    }
+    text = tempText;
+
+    if (text.length() <= 4){
+        for (unsigned int i {0}; i < text.length(); ++i){
+            position = charSet.indexOf(text.charAt(i));
+            if ( position > -1){
+            //Character found for translation
+            temp7SegData[3-i] = charLeds[position];
+            }
+            else{
+                displayable = false;
+                break;
+            }
+        }
+    }
+    else{
+        displayable = false;
+    }
+    
+    if (displayable){
+        for (int i {0}; i < 4; ++i)
+            _data[i] = temp7SegData[i] & tempDpData[i];  
+    }
+    return displayable;
+}
---
+++ src/FourBitLedDigitalTube.h
@@ -0,0 +1,64 @@
+#ifndef FourBitLedDigitalTube_h
+#define FourBitLedDigitalTube_h
+#include <Arduino.h>
+
+class TM74HC595LedTube{
+private:
+    int _sclk;
+    int _rclk;
+    int _dio;
+    int _data[4];
+    String charSet {"0123456789AabCcdEFGHIJLlnOoPqrSUY-_=~ "}; //for using indexOf() method
+    unsigned short int charLeds[38] = {
+    0xC0, //0
+    0xF9, //1
+    0xA4, //2
+    0xB0, //3
+    0x99, //4
+    0x92, //5
+    0x82, //6
+    0xF8, //7
+    0x80, //8
+    0x90, //9 
+    0x88, //A
+    0xA0, //a
+    0x83, //b
+    0xC6, //C
+    0xA7, //c
+    0xA1, //d
+    0x86, //E
+    0x8E, //F
+    0xC2, //G
+    0x89, //H
+    0xF9, //I
+    0xF1, //J
+    0xC7, //L
+    0xCF, //l
+    0xA9, //n
+    0xC0, //O
+    0xA3, //o
+    0x8C, //P
+    0x98, //q
+    0xAF, //r
+    0x92, //S
+    0xC1, //U
+    0x91, //Y
+    0xBF, //Minus -
+    0xF7,  //Underscore _
+    0xB7,  //Low =
+    0xB6,  //~ for Equivalent symbol
+    0xFF  //Space
+    };
+    void send(unsigned char content);
+public:
+    TM74HC595LedTube(int sclk, int rclk, int dio);
+    void clear();    
+    void send(unsigned char segments, unsigned char port);
+    bool print(String text);
+    void refresh();
+
+};
+
+
+
+#endif
