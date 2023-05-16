0dd7bc33ce07641e8aadf4e96c42c88e7c90f65a
Gaby Goldman <82763943+GabyGold67@users.noreply.github.com>
2023-05-16 15:55:59 +0300

    Support for the degrees character (º) added

--- src/FourBitLedDigitalTube.cpp
+++ src/FourBitLedDigitalTube.cpp
@@ -2,14 +2,13 @@
 #include <FourBitLedDigitalTube.h>
 
 TM74HC595LedTube::TM74HC595LedTube(int sclk, int rclk, int dio)
-    :_sclk {sclk}, _rclk {rclk}, _dio {dio} 
+    :_sclk{sclk}, _rclk{rclk}, _dio{dio}
 {
-  pinMode(_sclk, OUTPUT);
-  pinMode(_rclk, OUTPUT);
-  pinMode(_dio, OUTPUT);
-  clear();
+    pinMode(_sclk, OUTPUT);
+    pinMode(_rclk, OUTPUT);
+    pinMode(_dio, OUTPUT);
+    clear();
 }
-
 
 void TM74HC595LedTube::refresh(){
     static int firstRefreshed {0};
@@ -22,7 +21,7 @@ void TM74HC595LedTube::refresh(){
                 send(0xFF, 0b0010);
                 send(0xFF, 0b0100);
                 send(0xFF, 0b1000);
-                
+
                 _blinkTimer = millis();
             }
             else if((millis() - _blinkTimer)> _blinkRate){
@@ -51,77 +50,64 @@ void TM74HC595LedTube::refresh(){
             firstRefreshed = 0;
     }
 
-  return;
+    return;
 }
 
 void TM74HC595LedTube::clear(){
   //Cleans the contents of the internal DATA array display buffer (All leds off for all digits)
-  _digit[0]= _digit[1]= _digit[2]= _digit[3]= 0xFF;
-  refresh();
+  _digit[0] = _digit[1] = _digit[2] = _digit[3] = 0xFF;
+    refresh();
 }
 
-void TM74HC595LedTube::send(unsigned char content)
-{
-  //Sends the byte value (char <=> unsigned short int) to the 4 7-segment display bit by bit
+void TM74HC595LedTube::send(unsigned char content){
+    //Sends the byte value (char <=> unsigned short int) to the 4 7-segment display bit by bit
     shiftOut(_dio, _sclk, MSBFIRST, content);
 }
 
-void TM74HC595LedTube::send(unsigned char segments, unsigned char port)
+void TM74HC595LedTube::send(unsigned char segments, unsigned char port){
 // Sends the character 'X' to the digit 'port' of the display
 // Content and Port must be sent in two sequencial parts, character first, port second
-// so this overloaded two char send method uses the one char send method twice and then moves 
-//up the RCLK pin to present the content in the shift register
-{
-  digitalWrite(_rclk, LOW);
-  send(segments);
-  send(port);
-  digitalWrite(_rclk, HIGH);
+// so this overloaded two char send method uses the one char send method twice and then moves
+// up the RCLK pin to present the content in the shift register
+    digitalWrite(_rclk, LOW);
+    send(segments);
+    send(port);
+    digitalWrite(_rclk, HIGH);
 }
 
-void TM74HC595LedTube::fastSend(unsigned char content)
-{
-
-  for (int i {7}; i >= 0; i--)
-  {
-    if (content & 0x80)
-      digitalWrite(_dio, HIGH);
-    else
-      digitalWrite(_dio, LOW);
-    content <<= 1;
-    digitalWrite(_sclk, LOW);
-    digitalWrite(_sclk, HIGH);
-  }
-
+void TM74HC595LedTube::fastSend(unsigned char content){
+    for (int i {7}; i >= 0; i--){
+        if (content & 0x80)
+            digitalWrite(_dio, HIGH);
+        else
+            digitalWrite(_dio, LOW);
+        content <<= 1;
+        digitalWrite(_sclk, LOW);
+        digitalWrite(_sclk, HIGH);
+    }
 }
 
-void TM74HC595LedTube::fastSend(unsigned char segments, unsigned char port)
+void TM74HC595LedTube::fastSend(unsigned char segments, unsigned char port){
 // Sends the character 'X' to the digit 'port' of the display
 // Content and Port must be sent in two sequencial parts, character first, port second
-// so this overloaded two char send method uses the one char send method twice and then moves 
-//up the RCLK pin to present the content in the shift register
-{
-  digitalWrite(_rclk, LOW);
-  fastSend(segments);
-  fastSend(port);
-  digitalWrite(_rclk, HIGH);
+// so this overloaded two char send method uses the one char send method twice and then moves
+// up the RCLK pin to present the content in the shift register
+    digitalWrite(_rclk, LOW);
+    fastSend(segments);
+    fastSend(port);
+    digitalWrite(_rclk, HIGH);
 }
-
 
 bool TM74HC595LedTube::print(String text){
-    //Takes text and tries to display it at the LED tube
-    //returns true if found all the characters
-    //returns false if at least one char had no representation
-    //If no fidel representation is posible blanks the display to avoid false info risk
-  
-    bool displayable {true};
-    int position {-1};
-    String tempText {""};
-    int temp7SegData[4] {0xFF, 0xFF, 0xFF, 0xFF};
-    int tempDpData[4] {0xFF, 0xFF, 0xFF, 0xFF};
-  
-    //Finds out if there are '.' in the string to display, creates a mask to add them to the display
-    //and takes them out of the string to process the chars/digits
-    for (unsigned int i{0}; i< text.length(); ++i){
+    bool displayable{true};
+    int position{-1};
+    String tempText{""};
+    int temp7SegData[4]{0xFF, 0xFF, 0xFF, 0xFF};
+    int tempDpData[4]{0xFF, 0xFF, 0xFF, 0xFF};
+
+    // Finds out if there are '.' in the string to display, creates a mask to add them to the display
+    // and takes them out of the string to process the chars/digits
+    for(unsigned int i{0}; i < text.length(); ++i){
         if (text.charAt(i) != '.')
             tempText += text.charAt(i);
         else{
@@ -136,113 +122,113 @@ bool TM74HC595LedTube::print(String text){
     if (text.length() <= 4){
         for (unsigned int i {0}; i < text.length(); ++i){
             position = charSet.indexOf(text.charAt(i));
-            if (position > -1){
-                //Character found for translation
-                temp7SegData[3-i] = charLeds[position];
+            if (position > -1) {
+                // Character found for translation
+                temp7SegData[3 - i] = charLeds[position];
             }
-            else{
+            else {
                 displayable = false;
                 break;
             }
         }
     }
-    else{
+    else {
         displayable = false;
     }
-    
-    if (displayable){
-        for (int i {0}; i < 4; ++i)
-            _digit[i] = temp7SegData[i] & tempDpData[i];  
+
+    if (displayable) {
+        for (int i{0}; i < 4; ++i)
+            _digit[i] = temp7SegData[i] & tempDpData[i];
     }
     return displayable;
 }
 
-bool TM74HC595LedTube::print (const int &value, bool rgtAlgn, bool zeroPad){
-    bool displayable {true};
-    String readOut {""};    
+bool TM74HC595LedTube::print(const int &value, bool rgtAlgn, bool zeroPad){
+    bool displayable{true};
+    String readOut{""};
 
-    if((value < -999)||(value>9999)){
+    if ((value < -999) || (value > 9999)) {
         clear();
         displayable = false;
     }
-    else{
+    else {
         readOut = String(abs(value));
-        if (rgtAlgn){
+        if (rgtAlgn) {
             if (zeroPad)
                 readOut = "0000" + readOut;
             else
                 readOut = "    " + readOut;
 
-            if (value >= 0)            
+            if (value >= 0)
                 readOut = readOut.substring(readOut.length() - 4);
             else
                 readOut = readOut.substring(readOut.length() - 3);
         }
-        if(value<0)
+        if (value < 0)
             readOut = "-" + readOut;
         print(readOut);
     }
-    
+
     return displayable;
 }
 
-bool TM74HC595LedTube::print (const double &value, const unsigned int &decPlaces, bool rgtAlgn, bool zeroPad){
-    bool displayable {true};
-    String readOut {""};
-    String pad {""};
-    int start {0};
+bool TM74HC595LedTube::print(const double &value, const unsigned int &decPlaces, bool rgtAlgn, bool zeroPad){
+    bool displayable{true};
+    String readOut{""};
+    String pad{""};
+    int start{0};
 
     if (decPlaces == 0)
         displayable = print(int(value), rgtAlgn, zeroPad);
-    else if ((value < -999)||(value>9999)||(decPlaces > 4)){
+    else if ((value < -999) || (value > 9999) || (decPlaces > 4)) {
         displayable = false;
         clear();
     }
-    else if ((decPlaces + String(int(value)).length()) > (((value < 0) && (value > (-1))) ? 3 : 4)){    
+    else if ((decPlaces + String(int(value)).length()) > (((value < 0) && (value > (-1))) ? 3 : 4)) {
         displayable = false;
-        clear();        
+        clear();
     }
-    else{
-            if (value < 0 && value > -1)
-                readOut = "-";
-            readOut += String(int(value))+ ".";            
-            start = String(value).indexOf('.') + 1;
-            readOut += (String(value)+"0000").substring(start, start + decPlaces);            
+    else {
+        if (value < 0 && value > -1)
+            readOut = "-";
+        readOut += String(int(value)) + ".";
+        start = String(value).indexOf('.') + 1;
+        readOut += (String(value) + "0000").substring(start, start + decPlaces);
 
-            if (rgtAlgn){
-                if (readOut.length() < 5){
-                    if (value < 0)
-                        pad+= "-";
-                    if (zeroPad)
-                        pad += "0000";
-                    else
-                        pad += "    ";
-                    if(value<0)
-                        readOut = pad.substring(0,5-(readOut.length()-1)) + readOut.substring(1);
-                    else
-                        readOut = pad.substring(0,5-(readOut.length())) + readOut;
-                    readOut = readOut.substring(0,5);
-                }
+        if (rgtAlgn) {
+            if (readOut.length() < 5) {
+                if (value < 0)
+                    pad += "-";
+                if (zeroPad)
+                    pad += "0000";
+                else
+                    pad += "    ";
+                if (value < 0)
+                    readOut = pad.substring(0, 5 - (readOut.length() - 1)) + readOut.substring(1);
+                else
+                    readOut = pad.substring(0, 5 - (readOut.length())) + readOut;
+                readOut = readOut.substring(0, 5);
             }
-
-            displayable = print(readOut);
         }
+
+        displayable = print(readOut);
+    }
 
     return displayable;
 }
 
-bool TM74HC595LedTube::gauge (const int &level, char label){
-    bool displayable {true};
-    String readOut {""};
-    if((level < 0) || (level > 3)){
+bool TM74HC595LedTube::gauge(const int &level, char label){
+    bool displayable{true};
+    String readOut{""};
+    if ((level < 0) || (level > 3)) {
         clear();
         displayable = false;
     }
-    else{
+    else {
         readOut += label;
         if (readOut == "")
             readOut = " ";
-        switch (level){
+        switch (level) {
             case 0:
                 readOut += "   ";
                 break;
@@ -262,15 +248,15 @@ bool TM74HC595LedTube::gauge (const int &level, char label){
     return displayable;
 }
 
-bool TM74HC595LedTube::gauge (const double &level, char label){
-    bool displayable {true};
-    int intLevel {0};
+bool TM74HC595LedTube::gauge(const double &level, char label) {
+    bool displayable{true};
+    int intLevel{0};
 
-    if((level < 0.0) || (level > 1.0)){
+    if ((level < 0.0) || (level > 1.0)) {
         clear();
         displayable = false;
     }
-    else{
+    else {
         if (level < 0.25)
             intLevel = 0;
         else if (level < 0.50)
@@ -278,83 +264,86 @@ bool TM74HC595LedTube::gauge (const double &level, char label){
         else if (level < 0.75)
             intLevel = 2;
         else
-            intLevel = 3;        
+            intLevel = 3;
         displayable = gauge(intLevel, label);
     }
 
     return displayable;
 }
 
-void TM74HC595LedTube::begin(){
-  //Timer Interrupt 1 setup, frequency and prescaler should be modified to get
-  //the lowest acceptable (no disturbing flicker) display refresh rate
+void TM74HC595LedTube::begin()
+{
+    // Timer Interrupt 1 setup, frequency and prescaler should be modified to get
+    // the lowest acceptable (no disturbing flicker) display refresh rate
 
-  unsigned int freq01 = 900;
-  unsigned int prescaler01 = 256;
-  cli();//stop interrupts
+    unsigned int freq01 = 900;
+    unsigned int prescaler01 = 256;
+    cli(); // stop interrupts
 
-  //set timer1 interrupt at xxHz
-  TCCR1A = 0;// set entire TCCR1A register to 0
-  TCCR1B = 0;// same for TCCR1B
-  TCNT1  = 0;//initialize counter value to 0
-  // set compare match register for the selected frequency (defined by the freq and prescaler selected)
-  OCR1A = (((16*pow(10,6))/(prescaler01*freq01))-1);// = (16*10^6) / (P256 * 1024Hz) - 1 (must be <65536) = 624  P1024 the prescaler, the 25Hz the intended int frequency
-  // turn on CTC mode
-  TCCR1B |= (1 << WGM12);
-  
-  switch (prescaler01)
-  {
-  case 1:
-    // Set CS10 bit for 1 prescaler
-    TCCR1B |= (1 << CS10);  
-    break;
-  case 8:
-    // Set CS11 bit for 8 prescaler
-    TCCR1B |= (1 << CS11);  
-    break;
-  case 64:
-    // Set CS10 and CS11 bits for 64 prescaler
-    TCCR1B |= (1 << CS11) | (1 << CS10);  
-    break;
-  case 256:
-    // Set CS12 bit for 256 prescaler
-    TCCR1B |= (1 << CS12);  
-    break;
-  default:
-    // Set CS10 and CS12 bits for 1024 prescaler
-    TCCR1B |= (1 << CS12) | (1 << CS10);  
-    break;
-  }
+    // set timer1 interrupt at xxHz
+    TCCR1A = 0; // set entire TCCR1A register to 0
+    TCCR1B = 0; // same for TCCR1B
+    TCNT1 = 0;  // initialize counter value to 0
+    // set compare match register for the selected frequency (defined by the freq and prescaler selected)
+    OCR1A = (((16 * pow(10, 6)) / (prescaler01 * freq01)) - 1); // = (16*10^6) / (P256 * 1024Hz) - 1 (must be <65536) = 624  P1024 the prescaler, the 25Hz the intended int frequency
+    // turn on CTC mode
+    TCCR1B |= (1 << WGM12);
 
-  // enable timer compare interrupt
-  TIMSK1 |= (1 << OCIE1A);
+    switch (prescaler01) {
+        case 1:
+            // Set CS10 bit for 1 prescaler
+            TCCR1B |= (1 << CS10);
+            break;
+        case 8:
+            // Set CS11 bit for 8 prescaler
+            TCCR1B |= (1 << CS11);
+            break;
+        case 64:
+            // Set CS10 and CS11 bits for 64 prescaler
+            TCCR1B |= (1 << CS11);
+            TCCR1B |= (1 << CS10);
+            break;
+        case 256:
+            // Set CS12 bit for 256 prescaler
+            TCCR1B |= (1 << CS12);
+            break;
+        default:
+            // Set CS10 and CS12 bits for 1024 prescaler
+            TCCR1B |= (1 << CS12);
+            TCCR1B |= (1 << CS10);
+            break;
+    }
 
-  sei();//allow interrupts
+    // enable timer compare interrupt
+    TIMSK1 |= (1 << OCIE1A);
 
+    sei(); // allow interrupts
 }
-void TM74HC595LedTube::stop(){
+
+void TM74HC595LedTube::stop() {
     clear();
 
-    cli();//stop interrupts
+    cli(); // stop interrupts
 
-    //Clean timer1 interrupt setup
-    TCCR1A = 0;// set entire TCCR1A register to 0
-    TCCR1B = 0;// same for TCCR1B (disables all, including "CTC1 mode on", on the WGM12 bit)
-    TCNT1  = 0;//initialize counter value to 0
-    
-    OCR1A = 0;// set compare match register to 0 for no operation  
-    
-    TIMSK1 &= ~(1 << OCIE1A);// disable timer compare interrupt
+    // Clean timer1 interrupt setup
+    TCCR1A = 0; // set entire TCCR1A register to 0
+    TCCR1B = 0; // same for TCCR1B (disables all, including "CTC1 mode on", on the WGM12 bit)
+    TCNT1 = 0;  // initialize counter value to 0
 
-    sei();//allow interrupts
-  
+    OCR1A = 0; // set compare match register to 0 for no operation
+
+    TIMSK1 &= ~(1 << OCIE1A); // disable timer compare interrupt
+
+    sei(); // allow interrupts
+
     return;
 }
+
 bool TM74HC595LedTube::blink(){
     _blink = true;
     _blinkTimer = 0;
     _blinkShowOn = false;
-    
+
     return true;
 }
 
@@ -370,8 +359,8 @@ bool TM74HC595LedTube::isBlinking(){
     return _blink;
 }
 
-bool TM74HC595LedTube::setBlinkRate (const unsigned long &newRate){
-    if((newRate >= _minBlinkRate) && newRate <= _maxBlinkRate){
+bool TM74HC595LedTube::setBlinkRate(const unsigned long &newRate) {
+    if ((newRate >= _minBlinkRate) && newRate <= _maxBlinkRate) {
         _blinkRate = newRate;
         return true;
     }
@@ -379,37 +368,37 @@ bool TM74HC595LedTube::setBlinkRate (const unsigned long &newRate){
 }
 
 void TM74HC595LedTube::intRefresh(){
-    static int firstRefreshed {0};
+    static int firstRefreshed{0};
 
-    if (_blink == true){
+    if (_blink == true) {
         if (_blinkShowOn == false) {
-            if (_blinkTimer == 0){
-                //turn off all digits
+            if (_blinkTimer == 0) {
+                // turn off all digits
                 fastSend(0xFF, 0b0001);
                 fastSend(0xFF, 0b0010);
                 fastSend(0xFF, 0b0100);
                 fastSend(0xFF, 0b1000);
-                
+
                 _blinkTimer = millis();
             }
-            else if((millis() - _blinkTimer) > _blinkRate){
+            else if ((millis() - _blinkTimer) > _blinkRate) {
                 _blinkTimer = 0;
                 _blinkShowOn = true;
             }
         }
-        else{
-            if (_blinkTimer == 0){
+        else {
+            if (_blinkTimer == 0) {
                 _blinkTimer = millis();
             }
-            else if((millis() - _blinkTimer) > _blinkRate){
+            else if ((millis() - _blinkTimer) > _blinkRate) {
                 _blinkTimer = 0;
                 _blinkShowOn = false;
             }
         }
     }
 
-    if((_blink == false)||(_blinkShowOn == true)){
-        fastSend(_digit[firstRefreshed], 1<<firstRefreshed);
+    if ((_blink == false) || (_blinkShowOn == true)) {
+        fastSend(_digit[firstRefreshed], 1 << firstRefreshed);
         firstRefreshed++;
         if (firstRefreshed == 4)
             firstRefreshed = 0;
--- src/FourBitLedDigitalTube.h
+++ src/FourBitLedDigitalTube.h
@@ -2,87 +2,88 @@
 #define FourBitLedDigitalTube_h
 #include <Arduino.h>
 
-class TM74HC595LedTube{
+class TM74HC595LedTube
+{
 private:
-    const unsigned long _minBlinkRate {100};
-    const unsigned long _maxBlinkRate {2000};
+    const unsigned long _minBlinkRate{100};
+    const unsigned long _maxBlinkRate{2000};
     int _sclk;
     int _rclk;
     int _dio;
     int _digit[4];
-    bool _blink {false};
-    unsigned long _blinkTimer {0};
-    bool _blinkShowOn {false};
-    unsigned long _blinkRate {500};
-    String charSet {"0123456789AabCcdEeFGHhIiJLlnOoPqrStUuY-_=~ ."}; //for using indexOf() method
-    unsigned short int charLeds[44] = {
-    0xC0, //0
-    0xF9, //1
-    0xA4, //2
-    0xB0, //3
-    0x99, //4
-    0x92, //5
-    0x82, //6
-    0xF8, //7
-    0x80, //8
-    0x90, //9 
-    0x88, //A
-    0xA0, //a
-    0x83, //b
-    0xC6, //C
-    0xA7, //c
-    0xA1, //d
-    0x86, //E
-    0x84, //e
-    0x8E, //F
-    0xC2, //G
-    0x89, //H
-    0x8B, //h
-    0xF9, //I
-    0xFB, //i
-    0xF1, //J
-    0xC7, //L
-    0xCF, //l
-    0xAB, //n
-    0xC0, //O
-    0xA3, //o
-    0x8C, //P
-    0x98, //q
-    0xAF, //r
-    0x92, //S
-    0x87, //t
-    0xC1, //U
-    0xE3, //u
-    0x91, //Y
-    0xBF, //Minus -
-    0xF7,  //Underscore _
-    0xB7,  //Low =
-    0xB6,  //~ for Equivalent symbol
-    0xFF,  //Space
-    0x7F  //.
+    bool _blink{false};
+    unsigned long _blinkTimer{0};
+    bool _blinkShowOn{false};
+    unsigned long _blinkRate{500};
+    String charSet{"0123456789AabCcdEeFGHhIiJLlnOoPqrStUuY-_=~* ."}; // for using indexOf() method
+    unsigned short int charLeds[45] = {
+        0xC0, // 0
+        0xF9, // 1
+        0xA4, // 2
+        0xB0, // 3
+        0x99, // 4
+        0x92, // 5
+        0x82, // 6
+        0xF8, // 7
+        0x80, // 8
+        0x90, // 9
+        0x88, // A
+        0xA0, // a
+        0x83, // b
+        0xC6, // C
+        0xA7, // c
+        0xA1, // d
+        0x86, // E
+        0x84, // e
+        0x8E, // F
+        0xC2, // G
+        0x89, // H
+        0x8B, // h
+        0xF9, // I
+        0xFB, // i
+        0xF1, // J
+        0xC7, // L
+        0xCF, // l
+        0xAB, // n
+        0xC0, // O
+        0xA3, // o
+        0x8C, // P
+        0x98, // q
+        0xAF, // r
+        0x92, // S
+        0x87, // t
+        0xC1, // U
+        0xE3, // u
+        0x91, // Y
+        0xBF, // Minus -
+        0xF7, // Underscore _
+        0xB7, // Low =
+        0xB6, //~ for Equivalent symbol
+        0x9C, // °
+        0xFF, // Space
+        0x7F  //.
     };
     void send(unsigned char content);
     void fastSend(unsigned char content);
+
 public:
     TM74HC595LedTube(int sclk, int rclk, int dio);
     void begin();
     void stop();
     bool blink();
     bool noBlink();
-    void clear();    
+    void clear();
     void send(unsigned char segments, unsigned char port);
     void fastSend(unsigned char segments, unsigned char port);
-    bool print (String text);
-    bool print (const int &value, bool rgtAlgn = false, bool zeroPad = false);
-    bool print (const double &value, const unsigned int &decPlaces, bool rgtAlgn = false, bool zeroPad = false);
+    bool print(String text);
+    bool print(const int &value, bool rgtAlgn = false, bool zeroPad = false);
+    bool print(const double &value, const unsigned int &decPlaces, bool rgtAlgn = false, bool zeroPad = false);
     void refresh();
     void intRefresh();
-    bool gauge (const int &level, char label = ' ');
-    bool gauge (const double &level, char label = ' ');
-    bool setBlinkRate (const unsigned long &newRate);
+    bool gauge(const int &level, char label = ' ');
+    bool gauge(const double &level, char label = ' ');
+    bool setBlinkRate(const unsigned long &newRate);
     bool isBlinking();
 };
-
-
 
 #endif
